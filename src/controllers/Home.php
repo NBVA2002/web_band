e1c2eef6951d49ac64ee953306eea1d23e335a2d
NBVA2002 <nguyenbavietanh2002@gmail.com>
2023-12-22 18:40:23 +0700

    nbva update home page

--- src/controllers/Home.php
+++ src/controllers/Home.php
@@ -15,10 +15,11 @@ class Home extends Controller
         $dataList  = $this->model_tour->getListModel();
         $this->data['tour_list'] = $dataList;
 
-        $this->render('home/index',$this->data);
+        $this->render('home/index', $this->data);
     }
 
-    public function create() {
+    public function create()
+    {
         $currentDateTime = new DateTime();
         $formattedDateTime = $currentDateTime->format('Y-m-d H:i:s');
         $datar = [
@@ -33,7 +34,8 @@ class Home extends Controller
         return $this->model_tour->createModel($datar);
     }
 
-    public function update($id) {
+    public function update($id)
+    {
         $currentDateTime = new DateTime();
         $formattedDateTime = $currentDateTime->format('Y-m-d H:i:s');
         $datar = [
@@ -48,22 +50,25 @@ class Home extends Controller
         return $this->model_tour->updateModel($id, $datar);
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         return $this->model_tour->deleteModel($id);
     }
-    
-    public function list() {
-        $data= $this->model_tour->getListModel();
+
+    public function list()
+    {
+        $data = $this->model_tour->getListModel();
         echo '<pre>';
         echo print_r($data);
         echo '</pre>';
     }
 
-    public function detail($id) {
+    public function detail($id)
+    {
         $dataDetail  = $this->model_tour->getDetailModel($id);
 
         $this->data['tour_detail'] = $dataDetail;
-        $this->render('tour/detail',$this->data);
+        $this->render('tour/detail', $this->data);
     }
 
     public function send_mail()
@@ -72,11 +77,12 @@ class Home extends Controller
         $name = $_POST['name'];
         $message = $_POST['message'];
         $mail = new MailSender();
-        return $mail->sendMail('nguyenbavietanh2002@gmail.com','Contact', '
+        $mail->sendMail('nguyenbavietanh2002@gmail.com', 'Contact', '
         <h1>Thông tin liên hệ</h1>
-        <p>Email: '.$email.'</p>
-        <p>Name: '.$name.'</p>
-        <p>Message: '.$message.'</p>
+        <p>Email: ' . $email . '</p>
+        <p>Name: ' . $name . '</p>
+        <p>Message: ' . $message . '</p>
         ');
+        $this->render('home/index', $this->data);
     }
 }
